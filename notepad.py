bb74f4847ebc3d5999ca8e296c0ba09cfe8319d0
khadada <lkhadada@gmail.com>
2022-03-27 13:37:03 +0200

    All done with GUI, clear_text method, and executing line code

--- notepad.py
+++ notepad.py
@@ -18,4 +18,35 @@ class NotePad(QWidget):
         self.show_notepad_components()
     
     
+    def show_notepad_components(self):
+        """
+        Create widgets for notepad GUI and arrange the in window.
+        """
+        # Create push Buttons for editing menu
+        new_btn = QPushButton("New", self)
+        new_btn.move(10, 20)
+        new_btn.clicked.connect(self.clear_txt)
+        save_btn = QPushButton("Save", self)
+        save_btn.move(100, 20)
+        #new_btn.clicked.connect(self.save_txt)
+        
+        # Create text edit field:
+        self.text_field = QTextEdit(self)
+        self.text_field.resize(480, 300)
+        self.text_field.move(10, 60)
+    
+    def clear_txt(self):
+        """
+        If the new btn clicked, display asking user if they want to clear the text field or not.
+        """
+        confirm = QMessageBox.question(self,'Clear Confirm Message.','Are you sure want to clear text',QMessageBox.Yes,QMessageBox.No)
+        if confirm == QMessageBox.Yes:
+            self.text_field.clear()
+    
+    
+# Execution lines
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    note_pad = NotePad()   
+    sys.exit(app.exec_())
 
